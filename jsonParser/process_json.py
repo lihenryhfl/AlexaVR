262a1beb88612ee11e10155926160198d470b4e2
Peng <xiaochp@7831c1c06340.ant.amazon.com>
2016-07-16 04:41:44 -0700

    update json processing

--- jsonParser/process_json.py
+++ jsonParser/process_json.py
@@ -1,7 +1,19 @@
 import json
 import re
+
 def lambda_handler(event, context):
-    utterance = json.loads(event)["request"]["intent"]["slots"]["Utterance"]["value"].strip()
+    def crouch_action(utterance):
+        if 'crouch' in utterance:
+            return True
+        if 'hide' in utterance:
+            if 'under' in utterance or 'below' in utterance:
+                return True
+        return False
+
+
+    #utterance = json.loads(event)["request"]["intent"]["slots"]["Utterance"]["value"].strip()
+    utterance = json.loads(event)["request"]["intent"]["slots"]["Action"]["value"].strip()
+    key_values = {}
 
     objects = set(["couch", "table", "light", "bed", "stool", "chair", "fan", "wall"])
     directs = set(["left", "right", "forward", "backward", "around"])
@@ -20,7 +32,18 @@ def lambda_handler(event, context):
             break
 
     if action:
-        return [action, target, ""]
+        key_values["target"] = target
+        if crouch_action(utterance):
+            action = "GoAndCrouch"
+            key_values["action"] = action
+            key_values["unit"] = ""
+            return json.dumps(key_values)
+            #return [action, target, ""]
+
+        key_values["action"] = action
+        key_values["unit"] = ""
+        return json.dumps(key_values)
+        #return [action, target, ""]
 
     for direction in directs:
         if direction in utterance:
@@ -30,14 +53,31 @@ def lambda_handler(event, context):
 
     if action:
         distances = set(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"])
+        key_values["action"] = action
+        key_values["target"] = target
         for dist in distances:
             if dist in utterance:
                 unit = dist
-                return [action, target, unit]
-        return [action, target, ""]
+                key_values["unit"] = unit
+                return json.dumps(key_values)
+                #return [action, target, unit]
 
-    if 'crouch' in utterance:
+        key_values["unit"] = ""
+        return json.dumps(key_values)
+        #return [action, target, ""]
+
+    if crouch_action(utterance):
         action = "Crouch"
-        return [action, "", ""]
+        key_values["action"] = action
+        key_values["target"] = ""
+        key_values["unit"] = ""
+        #return [action, "", ""]
+    key_values["action"] = action
+    key_values["target"] = ""
+    key_values["unit"] = ""
+    return json.dumps(key_values)
+    #return ["", "", ""]
 
-    return ["", "", ""]
+f = open('./test.json')
+lines = [line.strip() for line in f.readlines()]
+print lambda_handler(' '.join(lines), "")
